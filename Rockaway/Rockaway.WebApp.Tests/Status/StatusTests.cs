2f325220819d6c54f9b0bace64d6bc1c4577cf3f
brycewoods <brycewoods94@gmail.com>
2023-10-16 22:06:18 +1000

    Fixed up the tests

--- Rockaway/Rockaway.WebApp.Tests/Status/StatusTests.cs
+++ Rockaway/Rockaway.WebApp.Tests/Status/StatusTests.cs
@@ -1,6 +1,6 @@
-using Rockaway.WebApp.Services;
-using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Rockaway.WebApp.Services;
 
 public class StatusTests {
 
@@ -19,11 +19,17 @@ public class StatusTests {
 		Modified = "MODIFIED"
 	};
 
+	private static ServerRestartStatus fakeRestartStatus = new() {
+		TimeSinceLastRestart = "15"
+	};
+
 	private class FakeStatusReporter : IStatusReporter {
 		public ServerStatus GetStatus() => fakeStatus;
+		public ServerRestartStatus GetTimeSinceLastRestart() => fakeRestartStatus;
 	}
 
 	private static readonly JsonSerializerOptions jsonSerializerOptions = new(JsonSerializerDefaults.Web);
+
 	[Fact]
 	public async Task Status_Returns_Correct_Json() {
 		var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
@@ -36,5 +42,19 @@ public class StatusTests {
 		var status = JsonSerializer.Deserialize<ServerStatus>(json, jsonSerializerOptions);
 		status.ShouldNotBeNull();
 		status.ShouldBeEquivalentTo(fakeStatus);
+	}
+
+	[Fact]
+	public async Task Restart_Status_Returns_Correct_Json() {
+		var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+				builder.ConfigureServices(
+				services => services.AddSingleton<IStatusReporter>(new FakeStatusReporter())
+			)
+		);
+		var client = factory.CreateClient();
+		var json = await client.GetStringAsync("/uptime");
+		var status = JsonSerializer.Deserialize<ServerRestartStatus>(json, jsonSerializerOptions);
+		status.ShouldNotBeNull();
+		status.ShouldBeEquivalentTo(fakeRestartStatus);
 	}
 }
--- Rockaway/Rockaway.WebApp.Tests/Usings.cs
+++ Rockaway/Rockaway.WebApp.Tests/Usings.cs
@@ -1,4 +1,4 @@
-global using Xunit;
 global using AngleSharp;
 global using Microsoft.AspNetCore.Mvc.Testing;
 global using Shouldly;
+global using Xunit;
